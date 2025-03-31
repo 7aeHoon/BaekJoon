c3f94701030a1bb04e66c1f45172797a90a7e67d
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-04-01 01:27:48 +0900

    [level 2] Title: 오픈채팅방, Time: 166.77 ms, Memory: 47.8 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -1,47 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
 #include <string>
 #include <vector>
-#include <map>
-#include <iostream>
-#include <sstream>
 
 using namespace std;
 
-vector<string> split(string s){
-    vector<string> a;
-    
-    stringstream its(s);
-    string buffer;
-    
-    while(getline(its,buffer,' '))
-        a.push_back(buffer);    
-    
-    return a;
+vector<string> split(string input, string delimiter) {
+    vector<string> result;
+    int pos = 0;
+    string token = "";
+
+    while ((pos = input.find(delimiter)) != string::npos) {
+        token = input.substr(0, pos);
+        result.push_back(token);
+        input.erase(0, pos + delimiter.size());
+    }
+
+    result.push_back(input);
+
+    return result;
 }
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
-    vector<string> infor, user, chat;
-    map<string,string> uid;
+    queue<pair<string, string>> q;
+    map<string, string> nickname;
 
-    for(int i=0; i<record.size(); i++){
-        infor=split(record[i]);
-        
-        if(infor[0]=="Enter"){
-            uid[infor[1]]=infor[2];
-            user.push_back(infor[1]);
-            chat.push_back("님이 들어왔습니다.");
-        }
-        else if(infor[0]=="Leave"){
-            user.push_back(infor[1]);
-            chat.push_back("님이 나갔습니다.");
-        }
-        else if(infor[0]=="Change"){
-            uid[infor[1]]=infor[2];
+    for (string str : record) {
+        vector<string> v = split(str, " ");
+
+        if (v[0] == "Enter") {
+            q.push({v[0], v[1]});
+            nickname[v[1]] = v[2];
+        } else if (v[0] == "Leave") {
+            q.push({v[0], v[1]});
+        } else {
+            nickname[v[1]] = v[2];
         }
     }
-    
-    for(int i=0; i<chat.size(); i++)
-        answer.push_back(uid[user[i]]+chat[i]);
-    
+
+    while (!q.empty()) {
+        pair<string, string> info = q.front();
+        q.pop();
+
+        if (info.first == "Enter") {
+            answer.push_back(nickname[info.second] + "님이 들어왔습니다.");
+        } else {
+            answer.push_back(nickname[info.second] + "님이 나갔습니다.");
+        }
+    }
+
     return answer;
 }
