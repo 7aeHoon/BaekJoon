de71c3c0e123b8f5078d15c02f150cfc52598a3f
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-04-01 20:47:26 +0900

    [level 2] Title: 타겟 넘버, Time: 10.03 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,45 +1,19 @@
-#include <algorithm>
 #include <iostream>
-#include <numeric>
-#include <string>
 #include <vector>
-
 using namespace std;
 
-vector<string> v;
-string str;
-int numbers_size;
 
-void go(int oper) {
-    if (str.size() == numbers_size) {
-        v.push_back(str);
-        return;
+int dfs(const vector<int>& numbers, int index, int sum, int target) {
+    if(index == numbers.size()) {
+        return (sum == target) ? 1 : 0;
     }
-
-    for (int i = 0; i < 2; i++) {
-        i ? str.push_back('+') : str.push_back('-');
-        go(i);
-        str.pop_back();
-    }
+    
+    int countAdd = dfs(numbers, index + 1, sum + numbers[index], target);
+    int countSub = dfs(numbers, index + 1, sum - numbers[index], target);
+    
+    return countAdd + countSub;
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    numbers_size = numbers.size();
-
-    for (int i = 0; i < 2; i++) {
-        i ? str.push_back('+') : str.push_back('-');
-        go(i);
-        str.pop_back();
-    }
-
-    for (string str : v) {
-        int sum = 0;
-        for (int i = 0; i < str.size(); i++) {
-            sum += (str[i] == '+') ? numbers[i] : -numbers[i];
-        }
-        if (sum == target) answer++;
-    }
-
-    return answer;
+    return dfs(numbers, 0, 0, target);
 }
