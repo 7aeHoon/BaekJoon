4230a130c874018a08ba8958796ca45e814a8eb8
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-04-14 23:07:47 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> DP(1005, 0);
+int price[1005];
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> price[i];
+    }
+
+    DP[0] = price[0] = 0;
+    DP[1] = price[1];
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= i; j++) {
+            DP[i] = max(DP[i], price[j] + DP[i - j]);
+        }
+    }
+
+    cout << DP[N] << '\n';
+
+    return 0;
+}
