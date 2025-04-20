6ddb28ccb173e2a8af40f1847c90f10d8045d062
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-04-21 01:00:22 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.75 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,39 +1,45 @@
+#include <algorithm>
+#include <iostream>
 #include <queue>
+#include <string>
 #include <vector>
 
 using namespace std;
 
+// 다리를 건너간 트럭의 수
+int completedTruckCnt;
+// 현재 다리가 견디고 있는 하중
+int currentBridgeWeight;
+// 다리를 건너고 있는 트럭의 정보를 저장하는 큐
+queue<pair<int, int>> bridgeQueue;
+// 대기 중인 트럭을 저장하는 큐
+queue<int> watingQueue;
+
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    
-    int idx=0;    //차량 지목용 idx
-    int sum=0;   //현재 다리에 올라와있는 차량 무게 총합
-    queue<int> q;  //현재 다리를 건너는 트럭 체크용 큐
-    
-    while(1){
-        
-        if(idx == truck_weights.size()){  //마지막 트럭일 때
-            answer += bridge_length;  //마지막 트럭이 지나는 시간 추가
-            break;  
-        }
-        
-        answer++; //시간초 증가
-        int tmp = truck_weights[idx];
-        
-        //차가 다리를 다 건넜을 경우
-        if(q.size() == bridge_length){
-            sum -= q.front();  //다 건넜으니 현재 다리에 있는 차들의 무게에서 제외
-            q.pop();  
-        }
-        
-        if(sum + tmp <= weight){  //다리에 다음 차가 진입할 수 있다면
-            sum += tmp;  //차량 무게 추가
-            q.push(tmp);  
-            idx++;  //다음 차량을 위해서
-        }else{   
-            q.push(0);  //진입할 수 없다면 0을 푸시해서 시간초 계산
-        }
+    int time = 0;
+
+    // 트럭를 대기 큐에 삽입
+    for (auto truck : truck_weights) {
+        watingQueue.push(truck);
     }
-   
-    return answer;
+
+    // 모든 트럭이 다리를 건널 때까지 진행
+    while (completedTruckCnt < truck_weights.size()) {
+        if (!bridgeQueue.empty() && bridgeQueue.front().second == time) {
+            currentBridgeWeight -= bridgeQueue.front().first;
+            bridgeQueue.pop();
+            completedTruckCnt++;
+        }
+
+        if (!watingQueue.empty() && currentBridgeWeight + watingQueue.front() <= weight) {
+            int truckWeight = watingQueue.front();
+            watingQueue.pop();
+            bridgeQueue.push({truckWeight, time + bridge_length});
+            currentBridgeWeight += truckWeight;
+        }
+
+        time++;
+    }
+
+    return time;
 }
