6a1aaff96551c6cb9254021031297ceb062d282a
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-04-23 23:43:07 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.74 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -6,11 +6,15 @@
 
 using namespace std;
 
-// HH:MM을 M으로 변환하는 함수
-int convertTimeToMinutes(string hhmm) { return stoi(hhmm.substr(0, 2)) * 60 + stoi(hhmm.substr(3, 2)); }
+// HH:MM을 총 M으로 변환하는 함수
+int convertTimeToMinutes(const string &hhmm) {
+    int h = stoi(hhmm.substr(0, 2));
+    int m = stoi(hhmm.substr(3, 2));
+    return h * 60 + m;
+}
 
 // input를 delimiter를 기준으로 구분하는 함수
-vector<string> splitRecord(string input, string delimiter) {
+vector<string> splitRecord(string input, const string &delimiter) {
     vector<string> result;
     int pos = 0;
     string token = "";
@@ -27,65 +31,67 @@ vector<string> splitRecord(string input, string delimiter) {
 }
 
 // 누적 주차 시간을 계산하여 요금을 측정하는 함수
-// fees는 (기본시간, 기본요금, 단위시간, 단위요금)
-int caculateParkingFee(vector<int> fees, int minutes) {
-    // 기본요금 청구
-    int result = fees[1];
+int caculateParkingFee(const vector<int> &fees, int minutes) {
+    // 기본 시간과 기본 요금
+    int baseTime = fees[0], bassFee = fees[1];
+    // 초과시간 당 초과 요금
+    int unitTime = fees[2], unitFee = fees[3];
+    // 요금
+    int total = bassFee;
     // 초과 시간
-    int overtime = minutes - fees[0];
+    int overtime = minutes - baseTime;
 
     // 초과 시간일 경우
     if (overtime > 0) {
-        result += (overtime / fees[2]) * fees[3];
+        total += (overtime / unitTime) * unitFee;
         // 초과 시간 올림
-        if (overtime % fees[2] > 0) {
-            result += fees[3];
+        if (overtime % unitTime > 0) {
+            total += unitFee;
         }
     }
 
-    return result;
+    return total;
 }
 
 vector<int> solution(vector<int> fees, vector<string> records) {
     vector<int> answer;
     // (차량번호, 시간)
-    map<string, string> parking;
+    map<string, string> parkingIn;
     // (차량번호, 사용시간)
-    map<string, int> fee;
+    map<string, int> totalTimeByCarNum;
 
     for (string record : records) {
         vector<string> v = splitRecord(record, " ");
-        // 시각
+
         string hhmm = v[0];
-        // 차량번호
         string carNum = v[1];
-        // IN/OUT 내역
         string status = v[2];
 
         // 출차일 경우
         if (status == "OUT") {
             // 출차 시간 - 입차 시간
-            int minutes = convertTimeToMinutes(hhmm) - convertTimeToMinutes(parking[carNum]);
+            int minutes = convertTimeToMinutes(hhmm) - convertTimeToMinutes(parkingIn[carNum]);
             // 차량 번호에 따라 사용시간 저장
-            fee[carNum] += minutes;
-            // 입출차 기록 제거
-            parking.erase(carNum);
+            totalTimeByCarNum[carNum] += minutes;
+            // 입차 기록 제거
+            parkingIn.erase(carNum);
         } else {  // 입차일 경우
             // 차량번호에 따른 입차시간 저장
-            parking[carNum] = hhmm;
+            parkingIn[carNum] = hhmm;
         }
     }
 
-    for (auto p : parking) {
+    // 입차 기록만 있는 차량 요금처리
+    for (auto p : parkingIn) {
         // 출차 시간 - 입차 시간
         int minutes = convertTimeToMinutes("23:59") - convertTimeToMinutes(p.second);
         // 차 번호에 따라 주차 요금 저장
-        fee[p.first] += minutes;
+        totalTimeByCarNum[p.first] += minutes;
     }
 
-    for (auto f : fee) {
-        // 요금 저장
-        answer.push_back(caculateParkingFee(fees, f.second));
+    // 차량번호에 따른 최종 요금 반환
+    for (auto info : totalTimeByCarNum) {
+        answer.push_back(caculateParkingFee(fees, info.second));
     }
 
     return answer;
