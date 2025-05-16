6b50bdf22e12ef01eb480afbd8286644b6c861e2
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-05-17 00:08:01 +0900

    [Gold IV] Title: 좋다, Time: 24 ms, Memory: 79688 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.swift
+++ 백준/Gold/1253. 좋다/좋다.swift
@@ -1,27 +1,34 @@
 import Foundation
 
-let N = Int(readLine()!)!
-let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+// N개의 수
+let N: Int = Int(readLine()!)!
 
-var result = 0
+// N개의 수를 가지는 오름차순 배열
+// 투 포인터는 정렬이 필요
+let arr: [Int] = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
 
-for target in arr.indices {
-    var start = 0
-    var end = arr.count - 1
+// 좋은 수의 개수
+var result: Int = 0
+
+for i in 0..<N {
+    // 왼쪽 포인터 l, 오른쪽 포인터 r
+    var l = 0
+    var r = N - 1
     
-    while start < end {
-        if arr[start] + arr[end] == arr[target] {
-            if start != target && end != target {
+    while l < r {
+        if arr[l] + arr[r] < arr[i] {
+            l += 1
+        } else if arr[l] + arr[r] > arr[i] {
+            r -= 1
+        } else {
+            if l != i && r != i {
                 result += 1
                 break
             }
-            if start == target { start += 1 }
-            if end == target { end -= 1 }
-        } else {
-            (arr[start] + arr[end] > arr[target]) ? (end -= 1) : (start += 1)
+            if l == i { l += 1 }
+            if r == i { r -= 1 }
         }
     }
-    
 }
 
 print(result)
