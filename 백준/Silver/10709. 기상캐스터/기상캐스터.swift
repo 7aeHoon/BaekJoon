c1abc36d6b1d2f740693dd9102de534e7a5b8112
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-06-02 00:51:03 +0900

    [Silver V] Title: 기상캐스터, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.swift
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.swift
@@ -1,32 +1,26 @@
 import Foundation
 
-let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
-let H = firstLine[0], W = firstLine[1]
+let HW = readLine()!.split(separator: " ").map { Int($0)! }
+let H = HW[0]
+let W = HW[1]
 
-var grid = [String]()
+var sky = [String]()
+var result = Array(repeating: Array(repeating: -1, count: W), count: H)
+
 for _ in 0..<H {
-    grid.append(readLine()!)
+    sky.append(readLine()!)
 }
 
-var ans = Array(repeating: Array(repeating: -1, count: W), count: H)
-
 for i in 0..<H {
-    var currentTime = -1
-    let row = Array(grid[i])
-    for j in 0..<W {
-        if row[j] == "c" {
-            ans[i][j] = 0
-            currentTime = 0
-        } else {
-            if currentTime >= 0 {
-                currentTime += 1
-                ans[i][j] = currentTime
+    for s in sky[i].enumerated() {
+        if s.element == "c" {
+            for j in s.offset..<W {
+                result[i][j] = j - s.offset
             }
         }
     }
 }
 
 for i in 0..<H {
-    let line = ans[i].map { String($0) }.joined(separator: " ")
-    print(line)
+    print(result[i].map { String($0) }.joined(separator: " "))
 }
