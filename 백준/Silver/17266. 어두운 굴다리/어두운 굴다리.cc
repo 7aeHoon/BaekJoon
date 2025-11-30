5add8cf4b095e7152c6ba8e4f337b57d7a022397
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-11-30 17:45:26 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, answer;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N;
+    cin >> M;
+
+    vector<int> v(M);
+
+    for (int i = 0; i < M; i++) {
+        cin >> v[i];
+    }
+
+    // 가로등은 최소 1개이상 설치됨
+
+    answer = max(answer, v.front());
+    answer = max(answer, N - v.back());
+
+    for (int i = 1; i < M; i++) {
+        int diff = v[i] - v[i - 1];
+        if (diff % 2 == 0) {
+            answer = max(answer, diff / 2);
+        } else {
+            answer = max(answer, diff / 2 + 1);
+        }
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
