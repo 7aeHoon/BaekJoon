2c63297e2a54acc8501b78acf14875f662e938cd
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-01-14 20:32:42 +0900

    [Gold IV] Title: 빙산, Time: 112 ms, Memory: 3160 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, year, answer;
+int ice[301][301], sea[301][301];
+bool visited[301][301];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+// 빙산이 존재하는지 체크
+bool isIceZero() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (ice[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+// 빙하 주변 바다 사이즈 체크
+int getSeaSize(const int& y, const int& x) {
+    int result = 0;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        // 빙하 밖 영역 패스
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+        // 바다면 카운트 증가
+        if (ice[ny][nx] == 0) result++;
+    }
+
+    return result;
+}
+
+// 빙산 녹이기
+void meltIce() {
+    // 먼저 초기화
+    memset(sea, 0, sizeof(sea));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            // 바다일 경우 패스
+            if (ice[i][j] == 0) continue;
+            // 좌표 주변 바다 사이즈 저장
+            sea[i][j] = getSeaSize(i, j);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            // 바다가 없어 빙하를 녹일 수 없으면 패스
+            if (sea[i][j] == 0) continue;
+            // 빙하 녹이기
+            ice[i][j] = max(ice[i][j] - sea[i][j], 0);
+        }
+    }
+}
+
+// 빙하 영역 탐색
+
+void dfs(const int& y, const int& x) {
+    // 방문처리
+    visited[y][x] = true;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+        if (ice[ny][nx] == 0 || visited[ny][nx]) continue;
+        dfs(ny, nx);
+    }
+}
+
+int checkIce() {
+    int result = 0;
+
+    memset(visited, false, sizeof(visited));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            // 바다 또는 방문 지역 패스
+            if (ice[i][j] == 0 || visited[i][j]) continue;
+            dfs(i, j);
+            result++;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> ice[i][j];
+        }
+    }
+
+    while (true) {
+        // 녹일 빙산이 없으면 종료
+        if (isIceZero()) break;
+        // 빙산 분리 시작 카운트
+        year++;
+        // 빙산이 녹기 시작
+        meltIce();
+        // 빙산 영역 체크
+        if(checkIce() >= 2) {
+            answer = year;
+            break;
+        }
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
