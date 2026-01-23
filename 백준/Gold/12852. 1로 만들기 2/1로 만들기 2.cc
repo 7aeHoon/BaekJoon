aaeeb56b0a6891e0907b2167067c9aec60c045fb
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-01-23 18:59:12 +0900

    [Gold V] Title: 1로 만들기 2, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -3,46 +3,8 @@
 using namespace std;
 
 int N;
-int arr[1000005];
-int nextPos[1000005];
-
-int go(int num) {
-    // 기저사례
-    if (num == 1) return 0;
-
-    // 메모이제이션
-    if (arr[num] != -1) {
-        return arr[num];
-    }
-
-    int result = INT_MAX;
-
-    if (num % 3 == 0) {
-        int x = go(num / 3) + 1;
-        if (result > x) {
-            result = x;
-            nextPos[num] = num / 3;
-        }
-    }
-    if (num % 2 == 0) {
-        int x = go(num / 2) + 1;
-        if (result > x) {
-            result = x;
-            nextPos[num] = num / 2;
-        }
-    }
-    if (num - 1 > 0) {
-        int x = go(num - 1) + 1;
-        if (result > x) {
-            result = x;
-            nextPos[num] = num - 1;
-        }
-    }
-
-    arr[num] = result;
-
-    return arr[num];
-}
+vector<int> dp;
+vector<int> nextPos;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -50,10 +12,33 @@ int main() {
 
     cin >> N;
 
-    memset(arr, -1, sizeof(arr));
-    memset(nextPos, -1, sizeof(nextPos));
+    dp.resize(N + 1, INT_MAX);
+    nextPos.resize(N + 1, -1);
 
-    cout << go(N) << '\n';
+    dp[1] = 0;
+
+    for (int i = 2; i <= N; i++) {
+        if (i % 2 == 0) {
+            if (dp[i / 2] + 1 < dp[i]) {
+                dp[i] = dp[i / 2] + 1;
+                nextPos[i] = i / 2;
+            }
+        }
+        if (i % 3 == 0) {
+            if (dp[i / 3] + 1 < dp[i]) {
+                dp[i] = dp[i / 3] + 1;
+                nextPos[i] = i / 3;
+            }
+        }
+        if (i >= 1) {
+            if (dp[i - 1] + 1 < dp[i]) {
+                dp[i] = dp[i - 1] + 1;
+                nextPos[i] = i - 1;
+            }
+        }
+    }
+
+    cout << dp[N] << '\n';
 
     while (N != -1) {
         cout << N << ' ';
