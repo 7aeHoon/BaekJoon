7f750e0aebb50f876c115de7cea4411ededfea45
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-01-28 03:43:49 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.cc
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -3,8 +3,17 @@
 using namespace std;
 
 long long N, answer;
-int num[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 vector<long long> v;
+
+void go(long long n, int last) {
+    v.push_back(n);
+
+    for (int i = last - 1; i >= 0; i--) {
+        go(n * 10 + i, i);
+    }
+
+    return;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -17,14 +26,8 @@ int main() {
         return 0;
     }
 
-    for (int i = 1; i < (1 << 10); i++) {
-        string s;
-        for (int j = 0; j < 10; j++) {
-            if (i & (1 << j)) {
-                s += to_string(num[j]);
-            }
-        }
-        v.push_back(stoll(s));
+    for (int i = 9; i >= 0; i--) {
+        go(i, i);
     }
 
     sort(v.begin(), v.end());
