f9d1e98ec50ac158cf55c4fbb842f91b2ffec997
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-03-08 06:06:19 +0900

    [level 0] Title: 세균 증식, Time: 0.06 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int solution(int n, int t) {
+        int answer = 0;
+        answer = n * (int)Math.pow(2, t);
+        return answer;
+    }
+}
