ebb43d64f871cf4945739f82a6adc6f5746b58d5
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-03-24 23:09:01 +0900

    [level 3] Title: 야근 지수, Time: 27.63 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -2,26 +2,39 @@
 
 using namespace std;
 
-priority_queue<int> pq;
-
 long long solution(int n, vector<int> works) {
     long long answer = 0;
 
-    for (const int &work : works) {
+    // 남은 작업량을 저장할 우선순위 큐
+    priority_queue<int> pq;
+
+    // 남은 작업량의 총합
+    int sum = 0;
+
+    for (const int& work : works) {
         pq.push(work);
+        sum += work;
+    }
+
+    // 남은 작업량을 모두 처리할 수 있을 경우
+    if (sum <= n) {
+        return 0;
     }
 
     for (int i = 0; i < n; i++) {
-        if(pq.empty() || pq.top() <= 0) { return 0; }
-        int top = pq.top();
+        // 가장 오래 걸리는 작업
+        int work = pq.top();
         pq.pop();
-        pq.push(top - 1);
+        // 작업이 아직 남아있다면
+        if (work - 1 != 0) {
+            pq.push(work - 1);
+        }
     }
 
     while (!pq.empty()) {
-        int top = pq.top();
+        int work = pq.top();
         pq.pop();
-        answer += top * top;
+        answer += work * work;
     }
 
     return answer;
