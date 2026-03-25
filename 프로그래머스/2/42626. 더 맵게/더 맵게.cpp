b245dc1cb855d398d3a3b53f9066b6d5a2f1e206
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-03-25 19:56:46 +0900

    [level 2] Title: 더 맵게, Time: 237.78 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -1,40 +1,33 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    // 우선순위 큐: 기본이 최대 힙
-    priority_queue<int, vector<int>, greater<int>> pq;
     int answer = 0;
 
-    // 스코빌 지수를 우선순위 큐에 삽입
-    for (int num : scoville) {
-        pq.push(num);
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    // 음식의 스코빌 지수를 저장
+    for (const auto& s : scoville) {
+        pq.push(s);
     }
 
-    // 스코빌 지수 섞기
-    while (pq.top() < K) {
-        // 힙의 사이즈가 1이고 루트노드의 값이 K미만일 경우
-        if (pq.size() == 1)  return -1;
-
-        // 가장 맵지 않은 음식의 스코빌 지수
+    while (pq.size() >= 2 && pq.top() < K) {
+        // 첫 번째로 가장 맵지 않은 음식
         int first = pq.top();
         pq.pop();
-        // 두 번째로 맵지 않은 음식의 스코빌 지수
+
+        // 두 번째로 가장 맵지 않은 음식
         int second = pq.top();
         pq.pop();
 
-        // 새로운 스코빌 지수
+        // 섞은 스코빌 지수
         int newScoville = first + (second * 2);
-
         pq.push(newScoville);
-        // 섞은 수 증가
         answer++;
     }
+
+    if (pq.top() < K) answer = -1;
 
     return answer;
 }
