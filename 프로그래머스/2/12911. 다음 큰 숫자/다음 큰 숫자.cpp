304983fd8c9959319396da80c6ea2ed1eb315259
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-03-30 10:44:47 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,17 +1,12 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int getOneSize(string str) {
+int getOneCount(const string& s) {
     int result = 0;
 
-    for (char ch : str) {
-        if (ch == '1') {
-            result++;
-        }
+    for (const char& ch : s) {
+        if (ch == '1') result++;
     }
 
     return result;
@@ -21,21 +16,31 @@ string toBinary(int num) {
     string result = "";
 
     while (num != 1) {
-        result += to_string(num % 2);
+        result += (num % 2 == 0) ? '0' : '1';
         num /= 2;
     }
 
-    result += to_string(num);
+    result += '1';
+
     reverse(result.begin(), result.end());
 
     return result;
 }
 
 int solution(int n) {
+    // n보다 큰 자연수
     int answer = n + 1;
-    const int a = getOneSize(toBinary(n));
 
-    while( a != getOneSize(toBinary(answer))) {
+    // n을 이진수로 변환
+    string binaryStr = toBinary(n);
+    // 해당 이진수에서 1의 개수 조회
+    int oneCnt = getOneCount(binaryStr);
+
+    // n보다 큰 수를 1씩 증가
+    while (true) {
+        string str = toBinary(answer);
+        int cnt = getOneCount(str);
+        if (oneCnt == cnt) break;
         answer++;
     }
 
