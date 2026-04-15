5bd7e3c99f6a4614ed6f2ab2c5449af76539a970
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-04-15 10:37:18 +0900

    [Silver IV] Title: 큐 2, Time: 324 ms, Memory: 10424 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.cc
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -1,50 +1,41 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-queue<int> q; // 큐
-string command;    // 사용자 명령어
-int value, count;  // 입력받는 정수 값, 명령어 수
+int N, num;
+queue<int> q;
 
-void push() {
-    cin >> value;
-    q.push(value);
-}
-void pop(void) {
-    if(q.size() == 0){
-        cout << -1 << '\n';
-    } else {
-        cout << q.front() << '\n';
-        q.pop();
-    } 
-}
-void size(void) { cout << q.size() << '\n'; }
-void empty(void) { q.size() == 0 ? cout << 1 << '\n' : cout << 0 << '\n'; }
-void front(void) { q.size() == 0 ? cout << -1 << '\n' : cout << q.front() << '\n'; }
-void back(void) { q.size() == 0 ? cout << -1 << '\n' : cout << q.back() << '\n'; }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int main(void) {
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    // 주어지는 명령의 수
+    cin >> N;
 
-    cin >> ::count;
+    for (int i = 0; i < N; i++) {
+        string cmd;
+        // 주어지는 명령
+        cin >> cmd;
 
-    for (int i = 0; i < ::count; i++) {
-        cin >> command;
-        if (command == "push") {
-            push();
-        } else if (command == "pop") {
-            pop();
-        } else if (command == "size") {
-            size();
-        } else if (command == "empty") {
-            empty();
-        } else if (command == "front") {
-            front();
-        } else if (command == "back") {
-            back();
+        if (cmd == "push") {
+            int num;
+            cin >> num;
+            q.push(num);
+        } else if (cmd == "pop") {
+            if (!q.empty()) {
+                cout << q.front() << '\n';
+                q.pop();
+            } else {
+                cout << -1 << '\n';
+            }
+        } else if (cmd == "size") {
+            cout << q.size() << '\n';
+        } else if (cmd == "empty") {
+            cout << (q.empty() ? 1 : 0) << '\n';
+        } else if (cmd == "front") {
+            cout << (!q.empty() ? q.front() : -1) << '\n';
+        } else if (cmd == "back") {
+            cout << (!q.empty() ? q.back() : -1) << '\n';
         }
     }
 
