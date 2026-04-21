0b2c87590bf160a76ccb35e81e077a81c0ad9862
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-04-21 14:15:36 +0900

    [Silver IV] Title: 로프, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,32 +1,29 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-#include <tuple>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-
-    int N, weight = -1;
-    vector<int> v;
-
+    int N;
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
-        int num;
-        cin >> num;
-        v.push_back(num);
+    vector<int> ropes(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> ropes[i];
     }
 
-    sort(v.begin(), v.end(), greater<>());
+    // 로프의 최대 중량을 내림차순으로 정렬
+    sort(ropes.begin(), ropes.end(), greater<int>());
 
-    for (int i = 1; i <= v.size(); i++) {
-        weight = max(weight, v[i-1] * i);
+    int max_weight = 0;
+
+    // 각 로프를 사용할 경우의 최대 중량을 계산
+    for (int i = 0; i < N; ++i) {
+        max_weight = max(max_weight, ropes[i] * (i + 1));
     }
 
-    cout << weight << '\n';
+    cout << max_weight << endl;
 
     return 0;
 }
