3386ce02d7191aafc4b3912eb93f0ba0b9033e54
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-05-12 00:20:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 27.28 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
     unordered_map<string, int> m;
 
-    for (const string& name : completion) {
+    for (const string& name : participant) {
         m[name]++;
     }
 
-    for (const string& name : participant) {
-        if (m[name] >= 1) {
-            m[name]--;
-        } else {
-            return name;
+    for (const string& name : completion) {
+        m[name]--;
+    }
+
+    for (const auto& p : m) {
+        if (p.second > 0) {
+            return p.first;
         }
     }
 
