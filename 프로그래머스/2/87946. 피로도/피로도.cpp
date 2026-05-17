349245a495a6b005b54bb75eae0080e3dba13261
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-05-17 20:29:05 +0900

    [level 2] Title: 피로도, Time: 0.94 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -2,31 +2,29 @@
 
 using namespace std;
 
-bool visited[9];
-int result;
+int maxCnt = INT_MIN;
 
-// 유저의 현재 피로도, 던전별 최소 필요 피로도와 소모 피로도, 던전 탐험 수
-void go(const int &pirodo, const vector<vector<int>> &dungeons, const int &cnt) {
-    if (pirodo <= 0) {
-        return;
-    }
+void dfs(const int& remainHP, vector<vector<int>> &dungeons, vector<bool>& visited, const int& cnt) {
+    maxCnt = max(maxCnt, cnt);
 
-    result = max(result, cnt);
-    
     for (int i = 0; i < dungeons.size(); i++) {
-        if (pirodo < dungeons[i][0] || visited[i]) { continue; }
+        if (remainHP < dungeons[i][0] || visited[i]) continue;
+
         visited[i] = true;
-        go(pirodo - dungeons[i][1], dungeons, cnt + 1);
+        dfs(remainHP - dungeons[i][1], dungeons, visited, cnt + 1);
         visited[i] = false;
     }
 }
 
+// 던전 방문 순서에 따라 결과가 달라짐
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = 0;
+    int answer = -1;
 
-    go(k, dungeons, 0);
+    vector<bool> visited(dungeons.size(), false);
 
-    answer = result;
+    dfs(k, dungeons, visited, 0);
+
+    answer = maxCnt;
 
     return answer;
 }
