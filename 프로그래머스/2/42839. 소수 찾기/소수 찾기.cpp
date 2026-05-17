a0c1f3cf258448e119d836bc92abea66496f37ec
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-05-17 21:08:18 +0900

    [level 2] Title: 소수 찾기, Time: 0.61 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -2,49 +2,46 @@
 
 using namespace std;
 
-set<int> nums;
-bool visited[10];
+unordered_set<int> unSet;
 
-bool isPrime(int n) {
+bool isPrime(const int& num) {
+    if (num <= 1) return false;
 
-    if (n < 2) return false;
-
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
     }
 
     return true;
 }
 
-void dfs(string numbers, string current) {
-
-    // 숫자 생성
-    if (!current.empty()) {
-        nums.insert(stoi(current));
+void dfs(const string& str, const vector<char>& nums, vector<bool>& visited) {
+    if (!str.empty()) {
+        unSet.insert(stoi(str));
     }
 
-    for (int i = 0; i < numbers.size(); i++) {
-
+    for (int i = 0; i < nums.size(); i++) {
         if (visited[i]) continue;
-
         visited[i] = true;
-
-        dfs(numbers, current + numbers[i]);
-
+        dfs(str + nums[i], nums, visited);
         visited[i] = false;
     }
 }
 
 int solution(string numbers) {
-
-    dfs(numbers, "");
-
     int answer = 0;
 
-    for (int num : nums) {
+    vector<char> nums;
+    vector<bool> visited;
 
+    for (const char& ch : numbers) {
+        nums.push_back(ch);
+    }
+
+    visited.resize(nums.size(), false);
+
+    dfs("", nums, visited);
+
+    for (const int& num : unSet) {
         if (isPrime(num)) {
             answer++;
         }
