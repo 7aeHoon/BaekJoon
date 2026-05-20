5fc6b25196fa6689a14c302e666a4ff7e87aa73d
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2026-05-20 11:33:14 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.43 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -4,46 +4,55 @@ using namespace std;
 
 unordered_map<string, int> unMap;
 
-void dfs(const string& order, const int& courseSize, const int& index, const string& current) {
-    if (current.size() == courseSize) {
+void dfs(const string& order, const int& size, const int& index, const string& current) {
+    if(current.size() == size) {
         unMap[current]++;
         return;
     }
-
-    for (int i = index; i < order.size(); i++) {
-        dfs(order, courseSize, i + 1, current + order[i]);
+    
+    for(int i = index; i < order.size(); i++) {
+        dfs(order, size, i + 1, current + order[i]);
     }
 }
 
 vector<string> solution(vector<string> orders, vector<int> course) {
     vector<string> answer;
-
-    // 각 주문 정렬(예시로, AD와 DA는 같은 코스요리이므로)
-    for (string& order : orders) {
+    
+    // "AB"와 "BA"는 같은 코스 요리
+    for(string& order: orders) {
         sort(order.begin(), order.end());
     }
-
-    for (const int& courseSize : course) {
+    
+    // 원하는 코스 사이즈 코스 요리 만들기
+    for(const int& courseSize: course) {
+        int maxOrder = 0;
+        
         unMap.clear();
-
-        for (const string& order : orders) {
+        
+        // 단품요리 구성
+        for(const string& order: orders) {
             dfs(order, courseSize, 0, "");
         }
-
-        int maxCnt = 0;
-
-        for (const auto& p : unMap) {
-            maxCnt = max(maxCnt, p.second);
+        
+        // 가장 많이 주문한 코스 요리의 사이즈
+        for(const auto& p: unMap) {
+            maxOrder = max(maxOrder, p.second);
         }
-
-        for (const auto& p : unMap) {
-            if (maxCnt == p.second && p.second >= 2) {
+        
+        for(const auto& p: unMap) {
+            if(maxOrder == p.second && p.second >= 2) {
                 answer.push_back(p.first);
             }
         }
     }
-
+    
     sort(answer.begin(), answer.end());
-
+    
     return answer;
 }
+
+
+/*
+    1. orders 정렬
+
+*/
